c85f5e06e6acb99cd842aacee0418ab4990dbc77
Navin Parmar <navinparmar98@gmail.com>
2023-03-29 19:57:52 -0700

    Boilerplate code to test server

--- client.py
+++ client.py
@@ -5,4 +5,10 @@ import socket
 
 
 if __name__ == "__main__":
-    pass
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(("127.0.0.1", 2020))
+    data = sock.recv(1024).decode()  # receive response
+    sock.sendall('completed'.encode())
+
+    print('Received from server: ' + data)  # show in terminal
+
